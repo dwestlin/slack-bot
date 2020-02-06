c636f49cf56fb55ef4c8fab16c67d82396411495
Daniel Westlin <dawe1103@student.miun.se>
2020-02-06 16:32:29 +0100

    .

--- src/app.js
+++ src/app.js
@@ -9,11 +9,11 @@ const { validateRequest } = require('./helpers/validateRequest')
 
 // MIDDLEWARES
 app.use(bodyParser.json({
-  verify: rawBodySaver
+    verify: rawBodySaver
 }))
 app.use(bodyParser.urlencoded({
-  verify: rawBodySaver,
-  extended: true
+    verify: rawBodySaver,
+    extended: true
 }))
 
 // VALIDATES THAT REQUEST COMES FROM SLACK'S API
@@ -21,16 +21,16 @@ app.use(validateRequest)
 
 // ROUTES
 app.get("/", (req, res) => {
-  res.status(200).send("Welcome to the IBM slackbot. This is just an API.")
+    res.status(200).send("Welcome to the IBM slackbot. This is just an API.")
 })
 
 app.use("/api", apiRoutes)
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`
-  })
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on this server!`
+    })
 })
 
 module.exports = app
--- src/helpers/rawBodySaver.js
+++ src/helpers/rawBodySaver.js
@@ -4,7 +4,7 @@
 const rawBodySaver = (req,res,buf,encoding) =>{
     if (buf && buf.length) {
         req.rawBody = buf.toString(encoding || "utf8")
-      }
+    }
 }
 
 module.exports = {rawBodySaver}
