957c572f4b23a4c6affdd4c28dea39eff7795575
dwestlin <dawe1103@student.miun.se>
2020-03-19 11:02:43 +0100

    changed the auth process

--- src/controllers/oauth.js
+++ src/controllers/oauth.js
@@ -2,11 +2,13 @@ const qs = require("querystring");
 const axios = require("axios");
 require("dotenv").config();
 
+const url = "https://slack.com/api/oauth.v2.access";
+
 const installBot = async function(req, res) {
   try {
-    let url = `https://slack.com/oauth/v2/authorize?client_id=${process.env.SLACK_CLIENT_ID}&team=${process.env.SLACK_TEAM}&redirect_url=${process.env.SLACK_REDIRECT_URL}&scope=${process.env.SLACK_scopes}`;
+    let redirectUrl = `https://slack.com/oauth/v2/authorize?client_id=${process.env.SLACK_CLIENT_ID}&team=${process.env.SLACK_TEAM}&redirect_url=${process.env.SLACK_REDIRECT_URL}&scope=${process.env.SLACK_scopes}`;
 
-    res.status(302).redirect(url);
+    res.status(302).redirect(redirectUrl);
   } catch (error) {
     console.log("ERROR:", error);
     res.status(500).end();
@@ -19,26 +21,43 @@ const authProcess = async (req, res) => {
       return res.status(401).end();
     }
 
-    let code = req.query.code;
     let headers = {
       headers: {
         "Content-Type": "application/x-www-form-urlencoded"
       }
     };
 
-    let url = "https://slack.com/api/oauth.v2.access";
-    let data = qs.stringify({
-      client_id: process.env.SLACK_CLIENT_ID,
-      client_secret: process.env.SLACK_CLIENT_SECRET,
-      code: code
-    });
+    var data = {
+      form: {
+        client_id: process.env.SLACK_CLIENT_ID,
+        client_secret: process.env.SLACK_CLIENT_SECRET,
+        code: req.query.code
+      }
+    };
 
     axios
       .post(url, data, headers)
       .then(result => {
-        return res
-          .status(200)
-          .send(`Bot installed at ${result.data.team.name} workspace.`);
+        const { access_token, refresh_token, expires_in, error } = result.data;
+
+        console.log(result.data);
+
+        if (error) {
+          res.sendStatus(401);
+          console.log(error);
+          return;
+        }
+
+        axios
+          .post(`${url}/team.info`, qs.stringify({ token: access_token }))
+          .then(result => {
+            if (!result.data.error) {
+              res.redirect(`http://${result.data.team.domain}.slack.com`);
+            }
+          })
+          .catch(err => {
+            console.error(err);
+          });
       })
       .catch(err => {
         return res.status(500).end();
