6656cd3efb819421b3ecb2ecf31f9c69d10caac0
daniel <dwestlin@gmail.com>
2020-03-19 16:03:44 +0100

    oauth2

--- src/controllers/oauth.js
+++ src/controllers/oauth.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const url = "https://slack.com/api/oauth.v2.access";
 
-const installBot = async function(req, res) {
+const installBot = async function (req, res) {
   try {
     let redirectUrl = `https://slack.com/oauth/v2/authorize?client_id=${process.env.SLACK_CLIENT_ID}&team=${process.env.SLACK_TEAM}&redirect_url=${process.env.SLACK_REDIRECT_URL}&scope=${process.env.SLACK_scopes}`;
 
@@ -21,43 +21,25 @@ const authProcess = async (req, res) => {
       return res.status(401).end();
     }
 
+    let code = req.query.code;
     let headers = {
       headers: {
         "Content-Type": "application/x-www-form-urlencoded"
       }
     };
 
-    var data = {
-      form: {
-        client_id: process.env.SLACK_CLIENT_ID,
-        client_secret: process.env.SLACK_CLIENT_SECRET,
-        code: req.query.code
-      }
-    };
+    let data = qs.stringify({
+      client_id: process.env.SLACK_CLIENT_ID,
+      client_secret: process.env.SLACK_CLIENT_SECRET,
+      code: code
+    });
 
     axios
       .post(url, data, headers)
       .then(result => {
-        const { access_token, refresh_token, expires_in, error } = result.data;
-
-        console.log(result.data);
-
-        if (error) {
-          res.sendStatus(401);
-          console.log(error);
-          return;
-        }
-
-        axios
-          .post(`${url}/team.info`, qs.stringify({ token: access_token }))
-          .then(result => {
-            if (!result.data.error) {
-              res.redirect(`http://${result.data.team.domain}.slack.com`);
-            }
-          })
-          .catch(err => {
-            console.error(err);
-          });
+        return res
+          .status(302)
+          .redirect(`http://${result.data.team.name}.slack.com`);
       })
       .catch(err => {
         return res.status(500).end();
@@ -67,5 +49,6 @@ const authProcess = async (req, res) => {
     res.status(500).end();
   }
 };
+
 
 module.exports = { installBot, authProcess };
