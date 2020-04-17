021b91b9a1eca12e1e31c3c26d039cb8fca69497
daniel <dwestlin@gmail.com>
2020-04-17 15:42:06 +0200

    destrcutured some functions

--- src/routes/commands.js
+++ src/routes/commands.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 
 const { validateRequest } = require("../helpers/middlewares");
-const commands = require("../controllers/commands");
+const { getEvents, getInfo, getUsers, getJoke, getWeather, addBiography } = require("../controllers/commands");
 
-router.post("/events", validateRequest, commands.getEvents);
-router.post("/info", validateRequest, commands.getInfo);
-router.post("/users", validateRequest, commands.getUsers);
-router.post("/jokes", validateRequest, commands.getJoke);
-router.post("/weather", validateRequest, commands.getWeather);
-router.post("/biography", validateRequest, commands.addBiography);
+router.post("/events", validateRequest, getEvents);
+router.post("/info", validateRequest, getInfo);
+router.post("/users", validateRequest, getUsers);
+router.post("/jokes", validateRequest, getJoke);
+router.post("/weather", validateRequest, getWeather);
+router.post("/biography", validateRequest, addBiography);
 
 module.exports = router;
